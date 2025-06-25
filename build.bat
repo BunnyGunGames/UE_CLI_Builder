f6620c1015e1e327565d0101559abac42606dcba
Nathan Haskins <nathan@haskins.it>
2025-06-25 06:54:26 -0500

    refacotr build script

--- build.bat
+++ build.bat
@@ -1,11 +1,26 @@
 @echo off
 setlocal
 
-:: Full path to your .sln file
-set SLN="C:\UnrealProjects\CursorCPP\CursorCPP.sln"
+:: Configure section, modify these variables to setup.
 
-:: Full path to MSBuild
+:: 1) Full path to MSBuild
 set MSBUILD="C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe"
+
+:: 2) Full path to Unreal Engine
+set UE_PATH="C:\Program Files\Epic Games\UE_5.5\Engine\Binaries\Win64\UnrealEditor.exe"
+
+:: 3) ue Project directory
+set PROJECT_DIR="C:\UnrealProjects\CursorCPP"
+
+:: AUTO section, do not modify below this line.
+
+
+:: Extract project name from directory
+for %%i in (%PROJECT_DIR%) do set PROJECT_NAME=%%~ni
+
+:: Full path to your .sln file
+set SLN=%PROJECT_DIR%\%PROJECT_NAME%.sln
+
 
 :: Prompt user to confirm Unreal Engine is shut down
 set /p UE_CONFIRM="Have you shut down Unreal Engine? Y/n: "
@@ -23,10 +38,10 @@ if exist "Intermediate" rmdir /s /q "Intermediate"
 echo Cleanup complete!
 
 :: Execute build like Visual Studio does
-echo Building... (some red is normal)
+echo Building...
 %MSBUILD% %SLN% /t:Build /p:Configuration=Development /p:Platform=Win64
 
-echo Complete! Launching Unreal Engine...
-start "" "C:\Program Files\Epic Games\UE_5.5\Engine\Binaries\Win64\UnrealEditor.exe" "C:\UnrealProjects\CursorCPP\CursorCPP.uproject"
+echo Build Complete (some red is normal). Launching Unreal Engine... 
+start "" %UE_PATH% %PROJECT_DIR%\%PROJECT_NAME%.uproject
 
 endlocal
